d6a6945b201989f2dd66bceba5120c3ba046cc80
Akash Agrawal <akagr@outlook.com>
2021-06-19 23:08:43 +0530

    Initial Commit

---
+++ Doot/DootApp.swift
@@ -0,0 +1,20 @@
+//
+//  DootApp.swift
+//  Doot
+//
+//  Created by Akash Agrawal on 19/06/21.
+//
+
+import SwiftUI
+
+@main
+struct DootApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
